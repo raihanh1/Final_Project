4eb4df6cfdfa7a06a2db9cf5f74b7bca9bc360d2
leannerod <leanne.roddy@gmail.com>
2020-11-27 21:24:29 -0500

    adding weather service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BuoyDetailsComponent } from './components/buoy-details/buoy-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'buoy', component: BuoyDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavTopComponent } from './navigation/nav-top/nav-top.component';
 import { NavSideComponent } from './navigation/nav-side/nav-side.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BuoyDetailsComponent } from './components/buoy-details/buoy-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavTopComponent,
     NavSideComponent
+    BuoyDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
