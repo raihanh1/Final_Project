c052d89bc234a778bdb7c233f79faf40cfc407be
leannerod <leanne.roddy@gmail.com>
2020-12-05 20:02:47 -0500

    style for buoy page and function to route to a buoy detail page

--- src/app/pages/dashboard/lake-all/lake-all.component.ts
+++ src/app/pages/dashboard/lake-all/lake-all.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute,ParamMap } from '@angular/router';
+import { switchMap } from 'rxjs/operators'
+import { GlosService } from 'src/app/services/glos.service';
 
 @Component({
   selector: 'app-lake-all',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lake-all.component.scss']
 })
 export class LakeAllComponent implements OnInit {
+  buoyInformation;
+  buoy$;
+  selectedId;
+  // heroes = HEROES;
+  id;
 
-  constructor() { }
+
+  constructor(
+    private buoyService: GlosService, 
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.buoyService.getGlos().subscribe((response:any) => {
+      this.buoyInformation = response;
+      console.log(response);
+    });
+
+    
+    this.buoy$ = this.route.paramMap.pipe(
+      switchMap(params => {
+        this.selectedId = Number(params.get('id'));
+        return this.buoyService.getGlos();
+      })
+    );
   }
 
+  goToBuoy(i) {
+    this.router.navigate([`/buoyportal/buoy/${this.buoyInformation[i].id}`])
+  }
 }
