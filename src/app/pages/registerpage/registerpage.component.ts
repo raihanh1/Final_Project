3a678d51f7e8d8669112060f22214422e19f4cc1
austin <austinkandt@gmail.com>
2020-12-11 21:27:23 -0500

    Hooked up basic click for submit button on create profile page.

--- src/app/pages/registerpage/registerpage.component.ts
+++ src/app/pages/registerpage/registerpage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registerpage',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterpageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  createUser = () => {
+    // If we're using ZIP for finding recommended buoys, we'll need to create variable on localStorage
+    // window.localStorage.setItem('greatLakes_zipCode', '')
+    // Create favorite buoys in localStorage
+    window.localStorage.setItem('greatLakes_favBuoys', '')
+    // Redirect user to favorite buoy screen
+    this.router.navigateByUrl(`/favorite`);
   }
 
 }
