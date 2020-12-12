b415257a052ed4d4389a17f7e8e7f5601d8d12c4
austin <austinkandt@gmail.com>
2020-12-11 21:50:44 -0500

    Hooked up mobile nav logout button

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  isLoggedIn: string = window.localStorage.getItem('greatLakes_isLoggedIn');
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  logoutUser = () => {
+    // Remove loggedin variable from local storage
+    window.localStorage.removeItem('greatLakes_isLoggedIn')
+    // Redirect user to home screen
+    // Not using router because need to force a reload for the state to change
+    window.location.href = '/buoyportal/all-lakes';
+}
 
 }
