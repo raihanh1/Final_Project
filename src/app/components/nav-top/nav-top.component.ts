28ef7330b1fbb0f05d9c5c60cb129a0fafa1ef01
austin <austinkandt@gmail.com>
2020-12-11 20:12:25 -0500

    Created signedIn is true to localStorage when user clicks log in button. ngIf on top header for different logged in states

--- src/app/components/nav-top/nav-top.component.ts
+++ src/app/components/nav-top/nav-top.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-top.component.scss']
 })
 export class NavTopComponent implements OnInit {
+  isLoggedIn: string = window.localStorage.getItem('greatLakes_isLoggedIn');
 
   constructor() { }
 
   ngOnInit(): void {
+    // Set variable that is accessible from the HTML
+    // That variable will access the local storage
+    console.log(typeof this.isLoggedIn);
   }
+
+
 
 }
