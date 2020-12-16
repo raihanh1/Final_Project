39c237c72f7c4afdee570a2ecfcc1d84b685e4f5
austin <austinkandt@gmail.com>
2020-12-16 18:08:24 -0500

    Added favorites section to all-lakes page.

--- src/app/pages/dashboard/lake-all/lake-all.component.ts
+++ src/app/pages/dashboard/lake-all/lake-all.component.ts
@@ -3,6 +3,7 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 import { GlosService } from 'src/app/services/glos.service';
 import { DomSanitizer } from '@angular/platform-browser';
+import { WeatherForecastService } from 'src/app/services/weather-forecast.service';
 import { TestBed } from '@angular/core/testing';
 
 @Component({
@@ -20,6 +21,11 @@ export class LakeAllComponent implements OnInit {
   chosenBuoy;
   chosenVideoLink;
   windSpeed;
+  favorites;
+  favoritesLocation = [];
+  weatherInfo = [];
+  weatherDegress = [];
+
 
   lakeBounds: google.maps.MapRestriction = {
     latLngBounds: {
@@ -48,9 +54,13 @@ export class LakeAllComponent implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
     private sanitizer: DomSanitizer,
+    private weatherFinder: WeatherForecastService
   ) { }
 
   ngOnInit(): void {
+    // Get localStorage
+    this.favorites = JSON.parse(localStorage.getItem('favorites'));
+
     // Get GLOS API and set to buoyInformation
     this.buoyService.getGlos().subscribe((response:any) => {
       this.buoyInformation = response;
@@ -61,13 +71,39 @@ export class LakeAllComponent implements OnInit {
           this.buoysWithLinks.push(this.buoyInformation[i].webcamLink[0]);
           this.buoysWithLinkNames.push(this.buoyInformation[i].longName);
         }
+        
+        //Makes sure if favorites is not null
+        if(this.favorites && this.favorites.length !== 0){
+          //For loop is made to find the locations of the favorited buoys
+          for(var x = 0; x < this.favorites.length; x++){
+            if(this.buoyInformation[i].id == this.favorites[x].split(" ")[0]){
+              //Adds their location
+              this.favoritesLocation.push(this.buoyInformation[i].lat);
+              this.favoritesLocation.push(this.buoyInformation[i].lon)
+            }
+          }
+        }
+
       }
+      
       this.buoysWithLinks.splice(1,1);
       var numberChosen = Math.floor((Math.random() * 14));
       var linkParse = this.buoysWithLinks[numberChosen].split('/');
       this.chosenBuoy = this.buoysWithLinkNames[numberChosen];
       this.chosenVideoLink = "https://www.limnotechdata.com/stations/albums/" + linkParse[4] + "/" + linkParse[4] + "720p.mp4";
-    })
+
+      console.log(this.favoritesLocation);
+      //Goes through the locations and adds the information to the array    
+      for(var i = 0; i < this.favoritesLocation.length; i+=2){      
+        this.weatherFinder.getWeatherExtra(this.favoritesLocation[i], this.favoritesLocation[i+1], "imperial").subscribe(res =>{
+          this.weatherInfo.push(res);
+          //Uses the degree to direction function
+          this.weatherDegress.push(this.degToCompass(Number(res['wind']['speed'])));
+        });
+      }
+
+      
+    })   
 
     
     this.buoy$ = this.route.paramMap.pipe(
@@ -76,6 +112,16 @@ export class LakeAllComponent implements OnInit {
         return this.buoyService.getGlos();
       })
     );
+  }
+
+  routeToBuoy(buoy){
+    this.router.navigateByUrl(`/buoyportal/${buoy}`);
+  }
+
+  degToCompass(num) {
+    var val = Math.floor((num / 22.5) + 0.5);
+    var arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
+    return arr[(val % 16)];
   }
 
   sanitize(url){
--- src/app/services/weather-forecast.service.ts
+++ src/app/services/weather-forecast.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -13,4 +13,15 @@ export class WeatherForecastService {
   getWeather (lat,lon) {
     return this.http.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=7c2e5484f8998d0e2ca297b1fe5b7677`);
   }
+
+  getWeatherExtra = (lat, lon, imperial = "imperial"): any => {
+    let filterParam = new HttpParams();
+    filterParam = filterParam.set('lat', lat);
+    filterParam = filterParam.set('lon', lon);
+    filterParam = filterParam.set('units', imperial);
+    return this.http.get(
+      'http://api.openweathermap.org/data/2.5/weather?appid=a2eff9aa00866bdec6f039157000e1b7',
+      { params: filterParam }
+    );
+  }  
 }
