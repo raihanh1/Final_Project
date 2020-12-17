41de4c78082aaecb9bf2961cec094b25f0e9eec6
leannerod <leanne.roddy@gmail.com>
2020-12-16 21:54:31 -0500

    finished recreation

--- src/app/pages/dashboard/buoy-details/buoy-details.component.ts
+++ src/app/pages/dashboard/buoy-details/buoy-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { GlosService } from 'src/app/services/glos.service';
 import { WeatherForecastService } from 'src/app/services/weather-forecast.service';
@@ -9,17 +9,17 @@ import { DomSanitizer } from '@angular/platform-browser';
   templateUrl: './buoy-details.component.html',
   styleUrls: ['./buoy-details.component.scss'],
 })
-export class BuoyDetailsComponent implements OnInit {
+export class BuoyDetailsComponent implements OnInit, OnChanges {
   weatherForecast;
   buoyInformation;
   lon;
   lat;
   id;
   currentBuoy;
-  obsName;
-  waveHeight;
-  windSpeed;
-  waterTemp;
+  waveHeight = [5];
+  waterTemp = [56, 52, 50, 45, 55];
+  // waveHeightValue;
+  // waterTempValue;
   buoysWithLinks = [];
   buoysWithLinkNames = [];
   webcamStream;
@@ -44,13 +44,16 @@ export class BuoyDetailsComponent implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
     private sanitizer: DomSanitizer
-  ) { }
-    
-  ngOnInit(): void {    
+  ) {}
+
+  ngOnChanges(changes){
+    console.log(changes, "changes");
+  }
+
+  ngOnInit(): void {
     // Routing from the click event
     this.route.params.subscribe((params) => {
       this.id = params['id'];
-
 
       // Getting the GLOS API and using the long and lat to call the weather API
       this.buoyService.getGlos().subscribe((response: any) => {
@@ -59,35 +62,80 @@ export class BuoyDetailsComponent implements OnInit {
         this.currentBuoy = this.buoyInformation.find((x) => {
           return x.id === this.id;
         });
-        this.mapOptions.center = { lat: this.currentBuoy.lat, lng: this.currentBuoy.lon};
+        this.mapOptions.center = {
+          lat: this.currentBuoy.lat,
+          lng: this.currentBuoy.lon,
+        };
         console.log(this.currentBuoy, 'buoy information');
-        if(this.currentBuoy.webcamLink[0] !== undefined){
+        if (this.currentBuoy.webcamLink[0] !== undefined) {
           let webcam = this.currentBuoy.webcamLink[0].split('/');
-          this.webcamStream = "https://www.limnotechdata.com/stations/albums/" + webcam[4] + "/" + webcam[4] + "720p.mp4";
+          this.webcamStream =
+            'https://www.limnotechdata.com/stations/albums/' +
+            webcam[4] +
+            '/' +
+            webcam[4] +
+            '720p.mp4';
         }
-        
 
         this.weatherService
-        // Using the lat and lon from currentBuoy to get the weather for that buoy
+          // Using the lat and lon from currentBuoy to get the weather for that buoy
           .getWeather(this.currentBuoy.lat, this.currentBuoy.lon)
           .subscribe((response: any) => {
             this.weatherForecast = response;
           });
       });
     });
-
-
-    
-  
   }
 
+  swimConditions = () => {
+    const waterTempValue = this.waterTemp[
+      Math.floor(Math.random() * this.waterTemp.length)
+    ];
+    const waveHeightValue = this.waveHeight[
+      Math.floor(Math.random() * this.waveHeight.length)
+    ];
+    
+    if (waterTempValue > 70 && waveHeightValue < 3) {
+    
+      return true;
+    }
+  };
 
-  
-  sanitize(url){
+  kayakConditions = () => {
+    const waveHeightValue = this.waveHeight[
+      Math.floor(Math.random() * this.waveHeight.length)
+    ];
+    console.log(waveHeightValue, 'wave height kayak')
+    if (waveHeightValue < 3) {
+      
+      return true;
+    }
+    return false;
+  };
+
+  fishingConditions = () => {
+    const waterTempValue = this.waterTemp[
+      Math.floor(Math.random() * this.waterTemp.length)
+    ];
+    if (waterTempValue >=50 || waterTempValue <= 75) {
+      return true;
+    }
+  };
+
+  boatingConditions = () => {
+    const waveHeightValue = this.waveHeight[
+      Math.floor(Math.random() * this.waveHeight.length)
+    ];
+    if (waveHeightValue <= 5) {
+      console.log(waveHeightValue, 'boat wave height');
+      return true;
+    }
+  };  
+
+  sanitize(url) {
     return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
-  
-  
+
   toggleFavorite = (buoyId) => {
     let favorites = JSON.parse(window.localStorage.getItem('favorites'));
     if (!favorites) {
@@ -95,23 +143,21 @@ export class BuoyDetailsComponent implements OnInit {
     }
     if (favorites.includes(buoyId)) {
       // remove from favorites
-      const indexOfBuoyToRemove = favorites.findIndex((favorite) => favorite === buoyId);
+      const indexOfBuoyToRemove = favorites.findIndex(
+        (favorite) => favorite === buoyId
+      );
       favorites.splice(indexOfBuoyToRemove, 1);
       window.localStorage.setItem('favorites', JSON.stringify(favorites));
     } else {
       favorites.push(buoyId);
       window.localStorage.setItem('favorites', JSON.stringify(favorites));
     }
-  }
+  };
 
   isFavorite = (buoyId) => {
     let favorites = JSON.parse(window.localStorage.getItem('favorites'));
-    if(favorites) {
-      return favorites.includes(buoyId);
-    }  
-    if (favorites){
+    if (favorites) {
       return favorites.includes(buoyId);
     }
-  }
-
+  };
 }
